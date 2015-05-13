e7f7a890c13d3d6e709bb66ea48434ef4dad9305
赤木詠滋 <s1223077@cce.kanagawa-it.ac.jp>
2015-05-13 19:49:35 +0900

    firstCommit

---
+++ WpfApplication1/WpfApplication1/CLass/Tossing_a_coin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.CLass
+{
+    /// <summary>
+    /// じゃんけん用のクラス
+    /// 簡単に勝敗だせないかな～って
+    /// </summary>
+    class Tossing_a_coin
+    {
+
+        public string name;
+        Random rand = new Random();
+
+
+        public int Check(int num)
+        {
+            int check=0;//勝敗判定用、0でドロー、1で勝ち、2で負け
+            int random = (int)rand.Next(0, 3);
+            switch (random)
+            {
+                case 0 : name="グー！"; break;
+                case 1: name = "チョキ！"; break;
+                case 2: name = "パー！"; break;
+
+            }
+            switch(num-random)
+            {
+                case -1: check = 1; break;//勝ち
+                case 0:check=0 ; break;//Drow
+                case 2: check = 1; break;//勝ち
+                default: check = 2; break;//負け
+            }
+            return check;
+        }
+
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WOL(0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            WOL(1);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            WOL(2);
+        }
+
+        private void WOL(int num)
+        {
+            CLass.Tossing_a_coin TAC = new CLass.Tossing_a_coin();
+            int check = TAC.Check(num);
+            switch(check)
+            {
+                case 0: TextBlock1.Text = (TAC.name + "\n引き分け！もう一回勝負！"); break;
+                case 1: TextBlock1.Text = (TAC.name + "\n君の勝ちかぁ・・・、まだまだ！もう一回勝負！"); break;
+                case 2: TextBlock1.Text = (TAC.name + "\n残念！君の負けだよっ！まだまだだね・・・っ！"); break;
+            }
+        }
+
+    }
+}
